e4ca9a15665432258912f9fd47e3d4c3f4c29d4c
Sam Meech-Ward <sam@meech-ward.com>
2018-02-15 19:50:02 -0800

    Add appdidfinishlaunching to the app delegate

--- FamousPeople/AppDelegate.swift
+++ FamousPeople/AppDelegate.swift
@@ -21,5 +21,9 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
+  
+  func applicationDidFinishLaunching(_ application: UIApplication) {
+    
+  }
 }
 
